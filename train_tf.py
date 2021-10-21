2a4a755d1d4b19f72fddc1f4aa0e55f6af417c96
Johniel Bocacao <accounts@johniel.nz>
2021-10-21 23:16:01 +1300

    Getting somewhere with TF, clean out data

--- train_tf.py
+++ train_tf.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from matplotlib import pyplot as plt
 from matplotlib.image import imread
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
@@ -30,38 +31,90 @@ X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_st
 # X_train_200 = tf.image.resize(X_train, [200, 200])
 # X_val_200 = tf.image.resize(X_val, [200, 200])
 
-X_train_100 = tf.image.resize(X_train, [100, 100])
-X_val_100 = tf.image.resize(X_val, [100, 100])
+X_train_sm = tf.image.resize(X_train, [150, 150])
+X_val_sm = tf.image.resize(X_val, [150, 150])
 
 labeller = LabelEncoder().fit(y_train)
 y_train_proc = labeller.transform(y_train)
 y_val_proc = labeller.transform(y_val)
 
+###
+# Build models
+###
+
 mlp = models.Sequential()
 mlp.add(layers.Flatten())
-mlp.add(layers.Dense(250))
-mlp.add(layers.Dense(50))
+mlp.add(layers.Dense(256, activation="relu"))
+mlp.add(layers.Dense(128, activation="relu"))
 mlp.add(layers.Dense(3))
 
-mlp.compile(optimizer=optimizers.SGD(learning_rate=0.001, momentum=0.9),
+mlp.compile(optimizer=optimizers.Adam(),
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=['accuracy'])
 
-history0 = mlp.fit(X_train_100, y_train_proc, epochs=20, batch_size=5, verbose=1,
-                  validation_data=(X_val_100, y_val_proc))
+history0 = mlp.fit(X_train_sm, y_train_proc, epochs=20, batch_size=10, verbose=1,
+                   validation_data=(X_val_sm, y_val_proc))
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].plot(history0.history["loss"])
+ax[0].plot(history0.history["val_loss"])
+ax[0].set_title("Training and validation (orange) loss")
+ax[1].plot(history0.history["accuracy"])
+ax[1].plot(history0.history["val_accuracy"])
+ax[1].set_title("Training and validation (orange) accuracy")
 
 cnn1 = models.Sequential()
-cnn1.add(layers.Conv2D(24, 5, 1, activation='relu', input_shape=(100, 100, 3)))
+cnn1.add(layers.Conv2D(64, 5, activation='relu'))
 cnn1.add(layers.MaxPooling2D((2, 2)))
-cnn1.add(layers.Conv2D(4, 5, 1, activation='relu'))
+cnn1.add(layers.Conv2D(64, 3, activation='relu'))
+cnn1.add(layers.MaxPooling2D((2, 2)))
+cnn1.add(layers.Conv2D(16, 3, activation='relu'))
 cnn1.add(layers.MaxPooling2D((2, 2)))
 cnn1.add(layers.Flatten())
+cnn1.add(layers.Dense(256, activation='relu'))
 cnn1.add(layers.Dense(64, activation='relu'))
 cnn1.add(layers.Dense(3))
-
-cnn1.compile(optimizer=optimizers.SGD(learning_rate=0.001, momentum=0.9),
+cnn1.compile(optimizer=optimizers.Adam(learning_rate=0.0001),
              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
              metrics=['accuracy'])
+history1 = cnn1.fit(X_train_sm, y_train_proc, epochs=25, batch_size=10, verbose=1,
+                    validation_data=(X_val_sm, y_val_proc))
 
-history1 = cnn1.fit(X_train_100, y_train_proc, epochs=20, batch_size=5, verbose=1,
-                    validation_data=(X_val_100, y_val_proc))
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].plot(history1.history["loss"])
+ax[0].plot(history1.history["val_loss"])
+ax[0].set_title("Training and validation (orange) loss")
+ax[1].plot(history1.history["accuracy"])
+ax[1].plot(history1.history["val_accuracy"])
+ax[1].set_title("Training and validation (orange) accuracy")
+
+X_train_half = tf.image.random_crop(X_train, [len(X_train), 150, 150, 3])
+X_train_aug = tf.concat([X_train_sm,
+                         tf.cast(X_train_half, tf.float32)
+                         ], axis=0)
+y_train_aug = np.concatenate((y_train_proc,y_train_proc), axis=0)
+
+cnn2 = models.Sequential()
+cnn2.add(layers.Conv2D(64, 5, activation='relu'))
+cnn2.add(layers.MaxPooling2D((2, 2)))
+cnn2.add(layers.Conv2D(64, 3, activation='relu'))
+cnn2.add(layers.MaxPooling2D((2, 2)))
+cnn2.add(layers.Conv2D(16, 3, activation='relu'))
+cnn2.add(layers.MaxPooling2D((2, 2)))
+cnn2.add(layers.Flatten())
+cnn2.add(layers.Dense(256, activation='relu'))
+cnn2.add(layers.Dense(64, activation='relu'))
+cnn2.add(layers.Dense(3))
+cnn2.compile(optimizer=optimizers.Adam(learning_rate=0.0001),
+             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+             metrics=['accuracy'])
+history2 = cnn2.fit(X_train_aug, y_train_aug, epochs=25, batch_size=10, verbose=1,
+                    validation_data=(X_val_sm, y_val_proc))
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].plot(history2.history["loss"])
+ax[0].plot(history2.history["val_loss"])
+ax[0].set_title("Training and validation (orange) loss")
+ax[1].plot(history2.history["accuracy"])
+ax[1].plot(history2.history["val_accuracy"])
+ax[1].set_title("Training and validation (orange) accuracy")
