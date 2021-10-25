8d10111dd5a06d2dcd0a0cc2655fe4f183c4e618
Johniel Bocacao <accounts@johniel.nz>
2021-10-25 16:29:09 +1300

    Final submission version

--- test.py
+++ test.py
@@ -2,33 +2,46 @@ import os
 import tensorflow as tf
 import numpy as np
 from matplotlib.image import imread
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+import sys
 
-base_dir = "testdata"
-X_list = []
-y = []
-throwaway = []
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) == 2:
+        model_file = args[1]
+        base_dir = "testdata"
+        X_list = []
+        y = []
+        throwaway = []
 
-for class_type in sorted(os.listdir(base_dir)):
-    if class_type == ".DS_Store": continue
-    subdir = os.path.join(base_dir, class_type)
-    for filename in sorted(os.listdir(subdir)):
-        if filename == ".DS_Store": continue
-        filepath = os.path.join(subdir, filename)
-        image = imread(filepath)
-        if image.shape == (300, 300, 3):
-            X_list.append(image/255.0)
-            y.append(class_type)
-        else:
-            print(filepath, "is not size 300x300, image is:", image.shape)
-            throwaway.append(image)
+        for class_type in sorted(os.listdir(base_dir)):
+            if class_type == ".DS_Store": continue
+            subdir = os.path.join(base_dir, class_type)
+            for filename in sorted(os.listdir(subdir)):
+                if filename == ".DS_Store": continue
+                filepath = os.path.join(subdir, filename)
+                image = imread(filepath)[:, :, :3]/255.0
+                if image.shape[0] != image.shape[1]:
+                    image = tf.image.central_crop(image, central_fraction=1)
+                image = tf.image.resize(image, [100, 100]).numpy()
+                X_list.append(image)
+                y.append(class_type)
 
-assert y.index('cherry') < y.index('strawberry') < y.index('tomato'), "Class labels have not been loaded correctly," \
-                                                                      "ensure that the cherry, strawberry and tomato " \
-                                                                      "subdirectories are in alphabetical order."
-# Wrangle X, y datasets
-X = np.stack(X_list, axis=0)
-X_sm = tf.image.resize(X, [100, 100])
-labeller = LabelEncoder().fit(y)
-y_proc = labeller.transform(y)
+        assert y.index('cherry') < y.index('strawberry') < y.index('tomato'), \
+            "Class labels have not been loaded correctly," \
+            "ensure that the cherry, strawberry and tomato " \
+            "subdirectories are in alphabetical order."
+
+        # Wrangle X, y datasets
+        X = np.stack(X_list, axis=0)
+        labeller = LabelEncoder().fit(y)
+        y_proc = labeller.transform(y)
+
+        model = tf.keras.models.load_model(model_file)
+        model.evaluate(X, y_proc)
+
+        predictions = np.argmax(model.predict(X), axis=1)
+        cm = tf.math.confusion_matrix(y_proc, predictions)
+    else:
+        print("usage: test.py [model]")
+        print("e.g. : test.py model.h5")
